553e9f2aed994f90427d7d7d545eb0bd4884c087
Safin Ramil <RamilSafnab1996@gmail.com>
2017-10-25 18:23:15 +0300

    Fixing dark image

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -5,9 +5,9 @@
 #include <camera_info_manager/camera_info_manager.h>
 
 #include <opencv2/imgproc/imgproc.hpp>
-#include "opencv2/highgui/highgui.hpp"
-
+#include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <opencv2/xphoto.hpp>
 
 #include <sstream>
 #include <boost/assign/list_of.hpp>
@@ -48,16 +48,14 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
 
   image_transport::ImageTransport it(nh);
-  image_transport::CameraPublisher pub = it.advertiseCamera("image_raw", 1);
+  image_transport::CameraPublisher pub = it.advertiseCamera("image_raw", 5);
 
   ROS_INFO("Stream provider: %s", DEFAULT_DEVICE_NAME);
 
   lirs::V4L2Capture cap(DEFAULT_DEVICE_NAME);
 
   std::string camera_name = "camera";
-
-  std::string frame_id = "camera_optical_frame";
-
+  std::string frame_id = "cam2_frame_id";
   std::string camera_info_url;
 
   if (!cap.isOpened()) {
@@ -67,41 +65,63 @@ int main(int argc, char **argv)
 
   ROS_INFO("Opened stream, starting to publish");
 
-  void* rawFrame;
+  uchar* rawData;
+
+  std_msgs::Header header;
   sensor_msgs::ImagePtr msg;
   sensor_msgs::CameraInfo cam_info_msg;
-  std_msgs::Header header;
-  header.frame_id = frame_id;
+
   camera_info_manager::CameraInfoManager cam_info_manager(nh, camera_name, camera_info_url);
+
+  // init
   cam_info_msg = cam_info_manager.getCameraInfo();
+  header.frame_id = frame_id;
+
+  // to Image message
+  msg = boost::make_shared<sensor_msgs::Image>();
+  msg->height = cap.getHeight();
+  msg->width  = cap.getWidth();
+//  msg->step   = cap.getStep();
+  msg->is_bigendian = false;
+  msg->encoding     = sensor_msgs::image_encodings::RGB8;
+
+  msg->data.resize(cap.getStep() * cap.getHeight());
+
+  header.frame_id = "camera0_frame_id";
+  msg->header = header;
 
   ros::Rate r(DEFAULT_FPS);
 
   while (nh.ok()) {
 
-    if (pub.getNumSubscribers() > 0) {
+//    if (pub.getNumSubscribers() > 0) {
 
       auto status = cap.mainloop();
 
       if (status) {
 
-        rawFrame = cap.getCurrentFrameData();
+        rawData = (uchar*) cap.getCurrentFrameData();
 
-        cv::Mat frame = cv::Mat(cap.getHeight(), cap.getWidth(), CV_8UC1, rawFrame, cap.getStep());
+        cv::Mat cvFrame = cv::Mat(cap.getHeight(), cap.getWidth(), CV_8UC1, rawData);
 
-        msg = cv_bridge::CvImage(header, sensor_msgs::image_encodings::BAYER_GRBG8, frame).toImageMsg();
+        cv::Mat cvRgbFrame = cv::Mat(cap.getHeight(), cap.getWidth(), CV_8UC3);
 
-        if (cam_info_msg.distortion_model == "") {
-          cam_info_msg = getDefaultCAMInfo(msg);
-          cam_info_manager.setCameraInfo(cam_info_msg);
-        }
+        cv::cvtColor(cvFrame, cvRgbFrame, CV_BayerGR2RGB);
+
+        msg->data.assign(cvRgbFrame.data, cvRgbFrame.data + cvRgbFrame.rows * cvRgbFrame.step1()); // set ROS image data
+        msg->step = cvRgbFrame.step1();
+
+//        if (cam_info_msg.distortion_model.empty()) {
+//          cam_info_msg = getDefaultCAMInfo(msg);
+//          cam_info_manager.setCameraInfo(cam_info_msg);
+//        }
 
         pub.publish(*msg, cam_info_msg, ros::Time::now());
 
       }
 
       ros::spinOnce();
-    }
+//    }
 
     r.sleep();
   }
